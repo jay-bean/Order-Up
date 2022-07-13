e726dfdfa59a6e9a6bddab8e605229efbb61e171
Jay Hutts <janieehutts@gmail.com>
2022-07-13 14:43:26 -0600

    initial setup

---
+++ app/config.py
@@ -0,0 +1,7 @@
+import os
+
+class Configuration:
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = environ.get("DATABASE_URL") or \
+      "postgresql://order_up:9uCxydbt@localhost/order_up_dev"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
